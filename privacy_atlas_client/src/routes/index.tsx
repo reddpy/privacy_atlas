8452faa85a9740d249be64148d13f2ff4d1e9997
KJ <19541224+reddpy@users.noreply.github.com>
2025-08-21 14:37:39 -0400

    added animations for the index page

--- privacy_atlas_client/src/routes/index.tsx
+++ privacy_atlas_client/src/routes/index.tsx
@@ -20,26 +20,26 @@ export default function Home() {
   return (
     <main class="min-h-screen text-gray-700 relative overflow-hidden">
       <div class="pt-[17vh] flex flex-col items-center px-4 relative z-20">
-        <h1 class="max-6-xs  my-6 text-2xl text-center">
+        <h1 class="max-6-xs  my-6 text-2xl text-center animate-logo">
           <span class="font-apple-garamond-italic text-6xl text-sky-500">
             Atlas
           </span>
           <span class="font-bold text-[#B62E00]">.</span>
         </h1>
 
-        <p class="pb-5 font-normal text-3xl font-apple-garamond text-center">
+        <p class="pb-5 font-normal text-3xl font-apple-garamond text-center animate-subtitle">
           The Knowledge of the World, <br class="sm:hidden" /> at your
           fingertips
           <span class="font-bold text-[#B62E00]">.</span>
         </p>
 
-        <div class="w-full max-w-4xl mb-8">
+        <div class="w-full max-w-4xl mb-8 animate-chatbox">
           <ChatBox inputSetter={setInput} />
         </div>
       </div>
 
       <img
-        class="absolute top-[15vh] left-1/2 transform -translate-x-1/2 w-[600px] min-w-[600px] max-w-[600px] opacity-10 z-0 pointer-events-none"
+        class="absolute top-[15vh] left-1/2 transform -translate-x-1/2 w-[600px] min-w-[600px] max-w-[600px] opacity-10 z-0 pointer-events-none animate-atlas"
         src="/atlas.png"
         alt="Atlas bearing the weight of knowledge"
       />
