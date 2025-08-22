802acd5482552cbbb01425a0c70d7faa606c4321
KJ <19541224+reddpy@users.noreply.github.com>
2025-08-22 15:06:58 -0400

    created new chat page UI

--- privacy_atlas_client/src/routes/chat.tsx
+++ privacy_atlas_client/src/routes/chat.tsx
@@ -1,7 +1,39 @@
+import ChatBoxChat from "~/components/chatboxChat";
+
 const ChatPage = () => {
+  let contentRef: HTMLDivElement | undefined;
+
   return (
-    <div class="fade-in-page">
-      <p>New chat!</p>
+    <div class="fade-in-page flex flex-col p-1 h-screen gap-4 relative">
+      <div
+        id="chat_header"
+        class="glass fixed top-0 pointer-events-none border-none h-12 flex-shrink-0 w-full mx-auto flex items-center justify-center"
+      >
+        <p>
+          <span class="font-apple-garamond-italic text-4xl text-sky-500">
+            Atlas
+          </span>
+          <span class="font-bold text-[#B62E00]">.</span>
+        </p>
+      </div>
+
+      <div
+        id="chat_message_container"
+        class="w-full max-w-2xl mx-auto space-y-4 pb-32 pt-20 pl-4"
+      >
+        <div>Message 1</div>
+        <div>Message 2</div>
+        <div>Message 2</div>
+      </div>
+
+      <div
+        id="chat_box_container"
+        class="fixed bottom-0 left-0 right-0 pointer-events-none pb-2 bg-[#FFF7ED]"
+      >
+        <div class="w-full max-w-2xl mx-auto pointer-events-auto">
+          <ChatBoxChat />
+        </div>
+      </div>
     </div>
   );
 };
